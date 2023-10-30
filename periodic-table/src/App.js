f26a7c766b8ef35641303119b441a29063154f46
shivam02022002 <shivamgupta02022002@gmail.com>
2023-10-30 22:29:57 +0530

    Added a navbar and remove the unused files.

--- periodic-table/src/App.js
+++ periodic-table/src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { BsGithub } from "react-icons/bs";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="h-16 text-xl flex justify-around items-center border-b-2 border-gray-300">
+        <div className="logo font-bold text-2xl">Pt</div>
+        <div className="header">
+          <ul className="flex flex-row m-3 space-x-10">
+            <li className="cursor-pointer p-2 font-semibold">Home</li>
+            <li className="cursor-pointer p-2 font-semibold">About</li>
+            <li className="cursor-pointer p-2 font-semibold">Contact</li>
+          </ul>
+        </div>
+        <div className="social-links">
+          <ul className="flex flex-row space-x-4 items-center justify-center">
+            <div className="flex flex-row space-x-2 border-2 rounded-3xl p-1 border-gray-500">
+              <MdDarkMode className="cursor-pointer text-xl " />
+              <MdOutlineLightMode className="cursor-pointer text-xl " />
+            </div>
+            <a href="https://github.com/the-shivam-gupta" target="_blank">
+              <BsGithub className="cursor-pointer text-2xl" />
+            </a>
+            <a href="https://twitter.com/ShivamGupt97925" target="_blank">
+              <FaSquareXTwitter className="cursor-pointer text-2xl" />
+            </a>
+          </ul>
+        </div>
       </header>
     </div>
   );
